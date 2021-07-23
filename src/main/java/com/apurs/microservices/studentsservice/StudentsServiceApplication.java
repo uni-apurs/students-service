253fe5d317d0b5e744bffb8d45af8909c50fd181
Stefan Katic <katicstefan.coder@gmail.com>
2021-07-23 15:19:36 +0200

    first commit

---
+++ src/main/java/com/apurs/microservices/studentsservice/StudentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.studentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsServiceApplication.class, args);
+	}
+
+}
